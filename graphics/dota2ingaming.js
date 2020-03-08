6c21dce5a4be8b05653d53deaa275e5b093742cd
rolandviper <rolandviper@gmail.com>
2020-03-08 17:24:05 +0800

    added animations for D2 ingame should have d2 ingame and draft all sorted

---
+++ graphics/dota2ingaming.js
@@ -0,0 +1,63 @@
+import { gsap } from '../node_modules/gsap/index.js';
+
+const baseEl = document.getElementById('base');
+const nameplateEl = document.getElementById('nameplate');
+
+//The information of the teams playing
+const leftTeam = document.getElementById('leftteam');
+const rightTeam = document.getElementById('rightteam');
+
+//The information ingame on the top right
+const series = document.getElementById('series');
+const game = document.getElementById('game');
+const round = document.getElementById('round');
+
+const vs = document.getElementById('vs');
+const middle = document.getElementById('versus'); //VERSUS word
+
+const logoEl = document.getElementById('logo');
+const bgEl = document.getElementById('bg');
+const sponsorEl = document.getElementById('sponsor');
+
+nodecg.listenFor('playdota2ingame', (data) => {
+	leftTeam.innerHTML = data.leftTeam;
+	rightTeam.innerHTML = data.rightTeam;
+	series.innerHTML = data.bestOf;
+	game.innerHTML = data.game;
+	round.innerHTML = data.round;
+
+	const tl = gsap.timeline();
+
+	tl.from([ baseEl ], 1, { opacity: 0 });
+	tl.to([ baseEl ], 1, { opacity: 1 });
+
+	tl.from([ middle ], 1, { opacity: 0, width: 0 });
+	tl.from([ nameplateEl ], 1, { opacity: 0 });
+
+	tl.from([ series ], 1, { opacity: 0, width: 0 });
+	tl.from([ game ], 1, { opacity: 0, width: 0 });
+	tl.from([ round ], 1, { opacity: 0, width: 0 });
+
+	tl.from([ bgEl ], 1, { opacity: 0, width: 0 });
+
+	tl.to([ logoEl, sponsorEl ], 1, { opacity: 1 });
+});
+
+nodecg.listenFor('stopdota2ingame', () => {
+	const tl = gsap.timeline();
+
+	tl.to([ logoEl, sponsorEl ], 1, { opacity: 0 });
+	tl.to([ bgEl, vs ], 1, { opacity: 0, width: 0 });
+
+	tl.to([ baseEl ], 1, { opacity: 0 });
+
+	tl.call(() => {
+		leftTeam.innerHTML = '';
+		rightTeam.innerHTML = '';
+		series.innerHTML = '';
+		game.innerHTML = '';
+		round.innerHTML = '';
+	});
+
+	tl.set([ baseEl, vs, logoEl, sponsorEl, bgEl ], { opacity: '', width: '' });
+});
