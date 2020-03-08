5177e79989ea32544ef2c61bd4ddc40721e2c4a3
rolandviper <rolandviper@gmail.com>
2020-03-08 15:29:40 +0800

    added animations to the D2 overlays for ingame and draft

---
+++ dashboard/dota2draft.js
@@ -0,0 +1,29 @@
+const leftTeam = document.getElementById('leftteam');
+const rightTeam = document.getElementById('rightteam');
+const bestOf = document.getElementById('series');
+const game = document.getElementById('game');
+const round = document.getElementById('round');
+
+function play() {
+	const data = {
+		leftTeam: leftTeam.value,
+		rightTeam: rightTeam.value,
+		bestOf: bestOf.value,
+		game: game.value,
+		round: round.value
+	};
+
+	nodecg.sendMessage('playdota2draft', data);
+}
+
+function stop() {
+	const data = {
+		leftTeam: leftTeam.value,
+		rightTeam: rightTeam.value,
+		bestOf: bestOf.value,
+		game: game.value,
+		round: round.value
+	};
+
+	nodecg.sendMessage('stopdota2draft', data);
+}
